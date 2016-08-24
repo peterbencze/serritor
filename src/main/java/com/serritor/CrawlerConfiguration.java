eed6c9b9b8191cd840f547071c6f7b718a8c8a6f
peterbencze <benczepeter95@gmail.com>
2016-08-24 21:35:21 +0200

    Implementing WebDriverFactory

--- src/main/java/com/serritor/CrawlerConfiguration.java
+++ src/main/java/com/serritor/CrawlerConfiguration.java
@@ -1,33 +1,51 @@
 package com.serritor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Implements a configuration class to get and set 
- * all the necessary options for the crawler.
+ * Provides an interface to specify properties to be used by the crawler.
  * 
  * @author Krisztian Mozsi
+ * @author Peter Bencze
  */
 public class CrawlerConfiguration {
     
-    private WebDriverType webDriverType;
-    private Map<String, String> webDriverOptions;
     private boolean runInBackground;
     
-    public WebDriverType getWebDriverType() {
-        return webDriverType;
+    private CrawlerDriver crawlerDriver;
+    private String driverPath;
+    private Map<String, Object> desiredCapabilities;
+    
+    
+    public CrawlerConfiguration() {
+        runInBackground = false;
+        crawlerDriver = CrawlerDriver.HTML_UNIT_DRIVER;
+        desiredCapabilities = new HashMap<>();
     }
     
-    public void setWebDriverType(WebDriverType webDriverType) {
-        this.webDriverType = webDriverType;
+    public CrawlerDriver getCrawlerDriver() {
+        return crawlerDriver;
     }
     
-    public Map<String, String> getWebDriverOptions() {
-        return webDriverOptions;
+    public void setCrawlerDriver(CrawlerDriver crawlerDriver) {
+        this.crawlerDriver = crawlerDriver;
     }
     
-    public void setWebDriverOptions(Map<String, String> webDriverOptions) {
-        this.webDriverOptions = webDriverOptions;
+    public String getDriverPath() {
+        return driverPath;
+    }
+
+    public void setDriverPath(String driverPath) {
+        this.driverPath = driverPath;
+    }
+    
+    public Map<String, Object> getDesiredCapabilities() {
+        return desiredCapabilities;
+    }
+    
+    public void setDesiredCapabilities(Map<String, Object> desiredCapabilities) {
+        this.desiredCapabilities = desiredCapabilities;
     }
     
     public boolean getRunInBackground() {
@@ -37,5 +55,4 @@ public class CrawlerConfiguration {
     public void setRunInBackground(boolean runInBackground) {
         this.runInBackground = runInBackground;
     }
-    
 }
