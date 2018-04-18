5af58339995ac55f686e7be2b3ec9df0e06c2014
Peter Bencze <benczepeter95@gmail.com>
2018-04-18 23:25:32 +0200

    Add test for fixed crawl delay mechanism

---
+++ src/test/java/com/github/peterbencze/serritor/internal/FixedCrawlDelayMechanismTest.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright 2018 Peter Bencze.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.peterbencze.serritor.internal;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Test cases for <code>FixedCrawlDelayMechanism</code>.
+ * 
+ * @author Peter Bencze
+ */
+public class FixedCrawlDelayMechanismTest {
+    
+    private CrawlerConfiguration configuration;
+    private FixedCrawlDelayMechanism crawlDelayMechanism;
+    
+    @Before
+    public void initialize() {
+        configuration = Mockito.spy(new CrawlerConfiguration());
+        crawlDelayMechanism = new FixedCrawlDelayMechanism(configuration);
+    }
+    
+    @Test
+    public void testGetDelay() {
+        // The delay should be the same as in the configuration
+        Assert.assertEquals(configuration.getFixedCrawlDelayDurationInMillis(), crawlDelayMechanism.getDelay());
+    }
+}
